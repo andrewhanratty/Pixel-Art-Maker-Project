a8b106137a3dda6574e6ffe82a2083ffd2b222ef
Andrew Hanratty <drew.hanratty@gmail.com>
2018-01-25 13:43:26 -0800

    Changes to js file for creating grid

--- project-pixel-art-maker-starter/js/designs.js
+++ project-pixel-art-maker-starter/js/designs.js
@@ -3,17 +3,17 @@ const m = document.getElementById('input_width').value;
 // Select height input and its value
 const n = document.getElementById('input_height').value;
 // Select the table element to build upon
-const pixelGrid = document.getElementById('pixel_canvas');
+const canvas = document.getElementById('pixel_canvas');
 
-// Do the variable m and n (created above... globally) apply within this function?
+// Do the variables m and n (created above... globally) apply within this function?
 function makeGrid() {
   for(let i = 1; i <= m; i++){
     const row = document.createElement('tr');
+    canvas.appendChild(row);
     for(let j = 1; j <= n; j++){
       const cell = document.createElement('td');
-      row.appendChild(cell);
+      canvas.appendChild(cell);
     }
-    pixelGrid.appendChild(row);
   }
 }
 
