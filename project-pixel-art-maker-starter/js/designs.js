8f108f51788fa2ae68deff0840a5f2e3ea8e82e8
Andrew Hanratty <drew.hanratty@gmail.com>
2018-01-25 12:41:29 -0800

    Add all project starter files

---
+++ project-pixel-art-maker-starter/js/designs.js
@@ -0,0 +1,26 @@
+// Select width input and its value
+const m = document.getElementById('input_width').value;
+// Select height input and its value
+const n = document.getElementById('input_height').value;
+// Select the table element to build upon
+const pixelGrid = document.getElementById('pixel_canvas');
+
+// Do the variable m and n (created above... globally) apply within this function?
+function makeGrid() {
+  for(let i = 1; i <= m; i++){
+    const row = document.createElement('tr');
+    for(let j = 1; j <= n; j++){
+      const cell = document.createElement('td');
+      row.appendChild(cell);
+    }
+    pixelGrid.appendChild(row);
+  }
+}
+
+// Select size input
+const sizeInput = document.getElementById('sizePicker');
+// When size is submitted by the user, call makeGrid()
+sizeInput.addEventListener('submit', makeGrid);
+
+// Select color input
+const colorInput = document.querySelector('.color');
