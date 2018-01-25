da5cec3ca4f8406bb1a6af8bf99ff66535f74687
Kanishka <kanishkamakhija007@gmail.com>
2018-01-26 03:07:23 +0530

    fixed grid display

--- project-pixel-art-maker-starter/js/designs.js
+++ project-pixel-art-maker-starter/js/designs.js
@@ -1,26 +1,32 @@
 // Select width input and its value
-const m = document.getElementById('input_width').value;
+//const m = document.getElementById('input_width').value;
 // Select height input and its value
-const n = document.getElementById('input_height').value;
+//const n = document.getElementById('input_height').value;
 // Select the table element to build upon
 const pixelGrid = document.getElementById('pixel_canvas');
 
 // Do the variable m and n (created above... globally) apply within this function?
 function makeGrid() {
-  for(let i = 1; i <= m; i++){
-    const row = document.createElement('tr');
-    for(let j = 1; j <= n; j++){
-      const cell = document.createElement('td');
-      row.appendChild(cell);
+    console.log("inside makegrid");
+    var m = document.getElementById('input_height').value;
+    var n = document.getElementById('input_width').value; 
+    for(let i = 1; i <= m; i++){
+        const row = document.createElement('tr');
+        for(let j = 1; j <= n; j++){
+            const cell = document.createElement('td');
+            row.appendChild(cell);
+            }
+        pixelGrid.appendChild(row);
     }
-    pixelGrid.appendChild(row);
-  }
 }
 
 // Select size input
-const sizeInput = document.getElementById('sizePicker');
+//const sizeInput = document.getElementById('sizePicker');
 // When size is submitted by the user, call makeGrid()
-sizeInput.addEventListener('submit', makeGrid);
-
+//sizeInput.addEventListener('submit', makeGrid);
+document.getElementById('sizePicker').addEventListener('submit', function(evt){
+    evt.preventDefault();
+    makeGrid();
+});
 // Select color input
 const colorInput = document.querySelector('.color');
