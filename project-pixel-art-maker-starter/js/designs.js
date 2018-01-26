89fc0d424d43b1a10f4b9840894ed999a3b70b41
Andrew Hanratty <drew.hanratty@gmail.com>
2018-01-26 14:53:58 -0800

    Add event listener for cell color change

--- project-pixel-art-maker-starter/js/designs.js
+++ project-pixel-art-maker-starter/js/designs.js
@@ -1,26 +1,38 @@
-// Select width input and its value
-const m = document.getElementById('input_width').value;
-// Select height input and its value
-const n = document.getElementById('input_height').value;
-// Select the table element to build upon
-const canvas = document.getElementById('pixel_canvas');
 
-// Do the variables m and n (created above... globally) apply within this function?
+// Select the table element to build upon
+const pixelGrid = document.getElementById('pixel_canvas');
+
 function makeGrid() {
+  console.log("inside makegrid");
+  var m = document.getElementById('input_width').value;
+  var n = document.getElementById('input_height').value;
   for(let i = 1; i <= m; i++){
     const row = document.createElement('tr');
-    canvas.appendChild(row);
     for(let j = 1; j <= n; j++){
       const cell = document.createElement('td');
-      canvas.appendChild(cell);
-    }
+      row.appendChild(cell);
+      }
+    pixelGrid.appendChild(row);
   }
 }
 
-// Select size input
-const sizeInput = document.getElementById('sizePicker');
 // When size is submitted by the user, call makeGrid()
-sizeInput.addEventListener('submit', makeGrid);
+document.getElementById('sizePicker').addEventListener('submit', function(evt){
+// apply preventDefault because the page's default will be to page-refresh
+  evt.preventDefault();
+  makeGrid();
+});
 
 // Select color input
-const colorInput = document.querySelector('.color');
+let colorPicker = document.getElementById('colorPicker');
+
+// Alter the background color of a cell
+let changeCell = function(event){
+  event.cell.style.backgroundColor = 'colorPicker'.value;
+}
+
+cell.addEventListener('click', changeCell);
+
+//document.getElementById('clear_grid').addEventListener('click', function(evt){
+//  evt.pixelGrid.clear();
+//});
